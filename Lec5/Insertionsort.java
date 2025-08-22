2289ef5a7c787bea1157375ce51815ce32877255
Hitesh Malhotra <hiteshmalhotra0018@gmail.com>
2025-08-22 20:41:44 +0530

    Lec5

---
+++ Lec5/Insertionsort.java
@@ -0,0 +1,30 @@
+package Lec5;
+
+public class Insertionsort {
+public static void main(String args[]) {
+	int[] arr = { 3, 2, 1, 4, 5, 7 };
+	sort(arr);
+	for (int i = 0; i < arr.length; i++) {
+		System.out.print(arr[i] + " ");
+	}
+
+}
+
+public static void sort(int[] arr) {
+
+	for (int i = 1; i < arr.length; i++) {
+
+		int j = i - 1;
+		int item = arr[i];
+		while (j >= 0 && arr[j] > item) {
+			arr[j+1] = arr[j];
+			j--;
+		}
+		j++;
+		arr[j] = item;
+
+	}
+
+}
+
+}
