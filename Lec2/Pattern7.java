6e166d9280d52f36aa7c4f7765a02211139c1e18
Hitesh Malhotra <hiteshmalhotra0018@gmail.com>
2025-08-22 20:40:27 +0530

    lec2

---
+++ Lec2/Pattern7.java
@@ -0,0 +1,19 @@
+package Lec2;
+
+public class Pattern7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+			Scanner scn = new Scanner(System.in);
+			System.out.println("Enter the age");
+			int age = scn.nextInt();
+
+			System.out.println("Enter your lucky number");
+			int num = scn.nextInt();
+			System.out.println("your age is " + age);
+			System.out.println("Your number is " + num);
+
+		}
+	}
