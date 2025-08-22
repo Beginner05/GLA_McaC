5551b365815e82ab76dd67eb53ef033baf44f483
Hitesh Malhotra <hiteshmalhotra0018@gmail.com>
2025-08-22 20:42:20 +0530

    lec6

---
+++ Lec6/BinarySearch.java
@@ -0,0 +1,29 @@
+package Lec6;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int arr[]= {1,2,3,4,5,23};
+	int trgt=5;
+		int res=find(arr,trgt);
+	System.out.println(res);
+	}
+public static int find(int arr[],int trgt)
+{
+	int lo=0;int hi=arr.length-1;
+	while(lo<=hi)
+	{
+		int mid=(lo+hi)/2;
+		if(arr[mid]==trgt)return mid;
+		if(arr[mid]>trgt)
+		{
+			hi=mid-1;
+		}
+		else if(arr[mid]<trgt){
+			lo=mid+1;
+		}
+	}
+	return -1;
+}
+}
